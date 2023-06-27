8f6a3c4923b4ceaf835e06375bd0e783c38fdaf1
Teresia <teresiamwangi26@gmail.com>
2023-06-27 09:08:00 +0300

    Complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+taxi1 = Taxi.create(name: "Uber")
+taxi2 = Taxi.create(name: "Bolt")
+taxi3 = Taxi.create(name: "Taxify")
+
+
+passenger1 = Passenger.create(name: "Lil Durk")
+passenger2 = Passenger.create(name: "Busta Rhymes")
+passenger3 = Passenger.create(name: "Tyra Banks")
+
+Ride.create(taxi_id: taxi1.id, passenger_id: passenger2.id)
+Ride.create(taxi_id: taxi2.id, passenger_id: passenger3.id)
+Ride.create(taxi_id: taxi3.id, passenger_id: passenger1.id)
